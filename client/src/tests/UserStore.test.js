f18321e50b75d5e38f64158d2facf2b5b9787f34
goldi4ek <2002mdo@gmail.com>
2023-04-25 22:55:31 +0300

    added more tests to client

--- client/src/tests/UserStore.test.js
+++ client/src/tests/UserStore.test.js
@@ -17,4 +17,37 @@ describe('UserStore', () => {
     userStore.setUser(user)
     expect(userStore.user).toEqual(user)
   })
+
+  it('should set isAuth to false', () => {
+    userStore.setIsAuth(false)
+    expect(userStore.isAuth).toBe(false)
+  })
+
+  it('should set user to null', () => {
+    userStore.setUser(null)
+    expect(userStore.user).toBeNull()
+  })
+
+  it('should set user to undefined', () => {
+    userStore.setUser(undefined)
+    expect(userStore.user).toBeUndefined()
+  })
+
+  it('should set and get user with address', () => {
+    const user = { name: 'John', email: 'john@example.com', address: { street: '123 Main St', city: 'Anytown', state: 'CA' } }
+    userStore.setUser(user)
+    expect(userStore.user).toEqual(user)
+  })
+
+  it('should set and get user with age', () => {
+    const user = { name: 'John', email: 'john@example.com', age: 30 }
+    userStore.setUser(user)
+    expect(userStore.user).toEqual(user)
+  })
+
+  it('should set and get user with empty object', () => {
+    const user = {}
+    userStore.setUser(user)
+    expect(userStore.user).toEqual(user)
+  })
 })
